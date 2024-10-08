a106f13b8e634d849c3bb3ae0d9a45a778ff3347
Ashu Negi <anegi6723@gmail.com>
2024-10-08 11:44:36 +0530

    added controllers to their routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,9 +1,9 @@
 import {Router} from "express";
-
+import {registerUser, loginUser} from "../controllers/user.controllers"
 const router = Router();
 
-router.route("/login");
-router.route("/register");
+router.route("/login").post(loginUser);
+router.route("/register").post(registerUser);
 router.route("/add_to_activity");
 router.route("/get_all_activity");
 
