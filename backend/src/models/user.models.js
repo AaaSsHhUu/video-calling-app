394e0bdda988da7e0a3575f4a6a556e05f51383f
Ashu Negi <anegi6723@gmail.com>
2024-10-08 11:43:54 +0530

    encrypting pasword before saving and method for checking password

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
     name : {
@@ -19,5 +20,18 @@ const userSchema = new Schema({
     }
 })
 
+userSchema.pre("save", async function(next){
+    // password should change only if it is modified
+    if(!this.isModified("password")){
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = function(password){
+    return bcrypt.compare(password, this.password)
+}
+
 const User = mongoose.model("User", userSchema);
-export default User;
+export {User};
